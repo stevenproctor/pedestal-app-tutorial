cf2b3aeae74337f0405e18fc29a33f3bca7e62cc
Steven Proctor <steven.proctor@gmail.com>
2013-11-09 22:31:39 -0600

    pedestal tutorial: Part 1 - Making a Counter

--- tutorial-client/app/src/tutorial_client/behavior.clj
+++ tutorial-client/app/src/tutorial_client/behavior.clj
@@ -2,10 +2,10 @@
     (:require [clojure.string :as string]
               [io.pedestal.app.messages :as msg]))
 
-(defn set-value-transform [old-value message]
-  (:value message))
+(defn inc-transform [old_value _]
+  ((fnil inc 0) old_value))
 
 (def example-app
   {:version 2
-   :transform [[:set-value [:greeting] set-value-transform]]})
+   :transform [[:inc [:my-counter] inc-transform]]})
 
